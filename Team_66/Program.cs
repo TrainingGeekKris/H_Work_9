246b4c669ed8a556afea4cccc192784a0caf20c2
Kristina <petrovnina22@mail.ru>
2023-01-16 22:01:18 +0300

    commit_66

---
+++ Team_66/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите число");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int SumNumber (int start, int end)
+{
+    if (start==end)
+    {
+        return start;
+    }
+    if (start>end)
+    {
+        Console.WriteLine("Числа введены некорректно");
+        return(0);
+    }
+    return (start+SumNumber(start+1,end));
+}
+Console.WriteLine(SumNumber(m,n));
+
